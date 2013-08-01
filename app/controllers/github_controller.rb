b00cf18f2940db1951c70ebccde130f3c8038542
zmontesd <zmontesd@gmail.com>
2013-08-01 15:24:54 -0700

    set up env to include client id and client secret

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,18 +1,18 @@
 require 'github'
 class GithubController < ApplicationController
 
-  # attr_accessor :github
-  # private :github
+  attr_accessor :github
+  private :github
 
-  # def authorize
-  #   github  = Github.new client_id: '...', client_secret: '...'
-  #   address = github.authorize_url redirect_uri: 'http://...', scope: 'repo'
-  #   redirect_to address
-  # end
+  def authorize
+    github  = Github.new client_id: ENV['GITHUB_CLIENT_ID'], client_secret: ENV['GITHUB_SECRET']
+    address = github.authorize_url redirect_uri: 'http://...', scope: 'repo'
+    redirect_to address
+  end
 
-  # def callback
-  #   authorization_code = params[:code]
-  #   access_token = github.get_token authorization_code
-  #   access_token.token   # => returns token value
-  # end
+  def callback
+    authorization_code = params[:code]
+    access_token = github.get_token authorization_code
+    access_token.token   # => returns token value
+  end
 end
